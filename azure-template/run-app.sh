60406761d12479248b9f0297c92a00c0354ed137
ad-test-20170718 <nikolahs@gmail.com>
2017-07-22 14:05:33 -0400

    Correct typo in run-app.sh

--- azure-template/run-app.sh
+++ azure-template/run-app.sh
@@ -2,7 +2,7 @@
 
 REPOSITORY_URL='https://github.com/ad-test-20170718/docker-cloud-app.git'
 
-git clone REPOSITORY_URL
+git clone $REPOSITORY_URL
 cd docker-cloud-app
 
 docker build -t docker-cloud-app:latest ./
